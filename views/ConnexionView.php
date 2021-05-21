e1a9a0da9a599317ddc4dfa7e3d344946da4f461
VinceDS99 <72012294+VinceDS99@users.noreply.github.com>
2021-05-21 16:50:49 +0200

    Semaine 20

---
+++ views/ConnexionView.php
@@ -0,0 +1,24 @@
+<form action="<?=site_url("AuthentificationController/Login")?>" method="post"> 
+
+
+
+<div class="form-group">
+<div>
+        <label for="login">Mail</label>
+        <input type="text" id="login_id" name="login_name" placeholder="">
+    </div>  <br>
+    <div>
+        <label for="mdp">Mot de passe</label>
+        <input type="password" id="password_id" name="password_name" placeholder="">
+    </div>
+
+<br>
+
+
+<button type="submit" class="btn btn-dark">Connexion</button>
+</form>
+
+
+
+</body>
+</html>
